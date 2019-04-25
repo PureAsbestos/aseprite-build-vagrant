32faaf2cb2ba50c40d13e2a14861b82d5bc84632
PureAsbestos <aquajonah@gmail.com>
2019-04-24 19:58:37 -0500

    Update BUILD-ASEPRITE.bat

--- BUILD-ASEPRITE.bat
+++ BUILD-ASEPRITE.bat
@@ -1,8 +1,4 @@
 @echo off
 vagrant up
-vagrant ssh
-cd C:\
-call C:\scripts\build.bat
-cd C:\aseprite\build\dist
-copy aseprite.exe C:\vagrant_data /Y
-robocopy data C:\vagrant_data\data /MIR /NFL /NDL /NJH /NJS /NC /NS /NP
+vagrant ssh -c "cd C:\ ^&^& call C:\scripts\build.bat ^&^& cd C:\aseprite\build\dist ^&^& copy aseprite.exe C:\vagrant_data /Y ^&^& robocopy data C:\vagrant_data\data /MIR /NFL /NDL /NJH /NJS /NC /NS /NP"
+vagrant halt
